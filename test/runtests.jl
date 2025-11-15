31a14718192393ee8c6b0d24ffb7a7b7ceb2afd6
tz-lom <nuzhdin.urii@gmail.com>
2025-11-15 13:50:40 +0100

    Fix failing test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,27 @@
+using Test
 using TrimCheck
+using TrimCheck: validate
+# @check_calls(init = begin
+#         include(joinpath(@__DIR__, "funcs.jl"))
+#     end, verbose = true,
+#     foo(Int32),
+#     foo(String),
+#     foo(TypeUnstable),
+#     foo(TypeStable))
 
-@check_calls(init = begin
-        include(joinpath(@__DIR__, "funcs.jl"))
-    end, verbose = true,
-    foo(Int32),
-    foo(String),
-    foo(TypeUnstable),
-    foo(TypeStable))
+@testset "validate_function" begin
+
+    result = TrimCheck.validate_function(:(sin(Int)))
+    @test isempty(result.errors)
+    @test result.call == :(sin(Int))
+
+    result = TrimCheck.validate_function(:(sin(1)))
+    @test result.call == :(sin(1))
+    @test length(result.errors) == 1
+    @test occursin("Failed to run validation", result.errors[1])
+
+    result = TrimCheck.validate_function(:(maximum(Vector{Any})))
+    @test result.call == :(maximum(Vector{Any}))
+    @test length(result.errors) >= 1
+    @test occursin("unresolved call from statement", result.errors[1])
+end
